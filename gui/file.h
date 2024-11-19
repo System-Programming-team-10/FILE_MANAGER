3937c76bb7694867be7bd1759d1b17c1756237a2
pupuReady <130034324+changwoo-jeon@users.noreply.github.com>
2024-11-19 16:54:28 +0900

    Remove: gui

--- gui/file.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_H
-# define FILE_H
-void paste(); //from file.c
-void remove_file(const char* filepath); //from file.c
-void move_file(const char* destination, const char* filepath); //from file.c
-void cp_file(const char* destination, const char* filepath); //from file.c
-#endif
